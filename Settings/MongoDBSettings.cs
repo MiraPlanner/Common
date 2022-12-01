a49b43606ec12210024f7defa264ef35bd2e19c8
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-01 01:35:18 +0100

    Update Connection String to use Username and Password

--- Settings/MongoDBSettings.cs
+++ Settings/MongoDBSettings.cs
@@ -1,9 +1,27 @@
+using MassTransit.Futures.Contracts;
+
 namespace Mira_Common.Settings
 {
     public class MongoDbSettings
     {
-        public string Host { get; init; }
+        public MongoDbSettings(string? username, string? password, string? host)
+        {
+            Username = username;
+            Password = password;
+            Host = host;
+        }
+
+        private string? Username { get; }
+        private string? Password { get; }
+        private string? Host { get; }
         public int Port { get; init; }
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString => GetConnectionString();
+        
+        public string GetConnectionString()
+        {
+            if (Username == null || Password == null)
+                return $"mongodb://{Host}:{Port}";
+            return $"{Username}:{Password}@mongodb://{Host}:{Port}";
+        }
     }
 }
